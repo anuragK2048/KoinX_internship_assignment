48e7928bae6e758fc8c9947c1d71256b6bb77186
anuragK2048 <developeranurag111@gmail.com>
2025-01-11 04:06:19 +0530

    chore: added tailwind css default configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 // import "./App.css";
 import PriceChart from "./components/priceChart/PriceChart";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar />
       <PriceChart />
     </>
   );
